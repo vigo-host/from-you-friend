ebaacdb819910c298a656a2e9ea512edb890d7a5
vigo-host <rafii.alaafi@gmail.com>
2025-12-14 05:18:31 +0700

    Add event listener to open letter on click

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("openLetter").addEventListener("click", () => {
+  document.getElementById("letterClosed").classList.add("hidden");
+  document.getElementById("letterOpen").classList.remove("hidden");
+});
